06bd11a3d7488ca94964057a4afba51ea3a1128b
JayeshMulwani93 <jayesh.mulwani93@gmail.com>
2022-07-12 17:56:58 +0000

    Deploying to gh-pages from  @ 1d6a9b14fb3535e69e1bc2149161b71b02efa6f8 🚀

---
+++ precache-manifest.6ecc9c4983024e4dc6def0b7d3f3de86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "457345c9bff51728d50e",
+    "url": "/CinemaVerse/static/css/main.40b1845f.chunk.css"
+  },
+  {
+    "revision": "457345c9bff51728d50e",
+    "url": "/CinemaVerse/static/js/main.457345c9.chunk.js"
+  },
+  {
+    "revision": "dbf4ecfc02410b9ee2cf",
+    "url": "/CinemaVerse/static/css/1.a52cdd7c.chunk.css"
+  },
+  {
+    "revision": "dbf4ecfc02410b9ee2cf",
+    "url": "/CinemaVerse/static/js/1.dbf4ecfc.chunk.js"
+  },
+  {
+    "revision": "61d5bcc071621a314890",
+    "url": "/CinemaVerse/static/js/runtime~main.61d5bcc0.js"
+  },
+  {
+    "revision": "e5db9a0cd6bb7d66decf370264fc00b6",
+    "url": "/CinemaVerse/index.html"
+  }
+];
