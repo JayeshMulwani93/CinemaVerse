266616c341e6cdd3688620181e89a45ec119a6b0
JayeshMulwani93 <jayesh.mulwani93@gmail.com>
2022-07-16 13:56:01 +0000

    Deploying to gh-pages from  @ 4ce7c61bf54368256cb2d3de25b2c2e85d6474dc 🚀

---
+++ precache-manifest.c9d61fd3a241882b3ba9913b9251a711.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "22f8869f8c6c3026164c",
+    "url": "/CinemaVerse/static/css/main.efa721c0.chunk.css"
+  },
+  {
+    "revision": "22f8869f8c6c3026164c",
+    "url": "/CinemaVerse/static/js/main.22f8869f.chunk.js"
+  },
+  {
+    "revision": "7cc60ea65e23f6786fd4",
+    "url": "/CinemaVerse/static/css/1.bb92660e.chunk.css"
+  },
+  {
+    "revision": "7cc60ea65e23f6786fd4",
+    "url": "/CinemaVerse/static/js/1.7cc60ea6.chunk.js"
+  },
+  {
+    "revision": "61d5bcc071621a314890",
+    "url": "/CinemaVerse/static/js/runtime~main.61d5bcc0.js"
+  },
+  {
+    "revision": "9782bb3019c5fa8f3fff35ce937c934d",
+    "url": "/CinemaVerse/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/CinemaVerse/precache-manifest.45004b23de55cd1029bca91c47607916.js"
+  "/CinemaVerse/precache-manifest.c9d61fd3a241882b3ba9913b9251a711.js"
 );
 
 workbox.clientsClaim();
